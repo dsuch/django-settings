5ca0f45fe72f60e54fb52561e6417030dfb702ee
Dariusz Suchojad <dsuch-gitlab-com@m.zato.io>
2016-01-20 14:06:06 +0100

    Updating import for Django 1.7+ compatibility.

--- django_settings/models.py
+++ django_settings/models.py
@@ -2,7 +2,7 @@
 # framework
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import generic
+from django.contrib.contenttypes.fields.forms import GenericForeignKey
 from django.utils.translation import ugettext_lazy as _
 
 # app local
